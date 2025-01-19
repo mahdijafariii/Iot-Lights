ebe0ff186214e652f8e5f548917cd11605e9104f
Mahdi Jafari <mahdijm.bb@gmail.com>
2025-01-19 16:22:11 +0330

    add main method and sync with bot

--- Bot.py
+++ Bot.py
@@ -5,6 +5,8 @@ import threading
 class Bot:
     def __init__(self, token):
         self.token = token
+        self.user_message = ""
+        self.update = ""
 
     async def start(self, update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("این بات جهت کار با یکسری دستورات کنترلی آماده ساخته شده! خوش آمدید 💓")
@@ -13,18 +15,15 @@ class Bot:
         user_message = update.message.text
         await update.message.reply_text("درخواست شما با موفقیت ارسال شد✅")
         await update.message.reply_text("منتظر دریافت پاسخ از AI هستم...🔍")
+        self.user_message = user_message
+        self.update = update
 
-        # ایجاد یک thread برای پردازش پیام در پس‌زمینه
-        thread = threading.Thread(target=self.handle_message_in_background, args=(user_message, update))
+        thread = threading.Thread(target=self.handle_message_in_background)
         thread.start()
 
-    def handle_message_in_background(self, message, update):
-        # نمایش پیام در کنسول
-        print(f"پیام دریافت شده: {message}")
+    def handle_message_in_background(self):
+        return(self.user_message)
 
-        # برای این که در پیام تلگرام ارسال شود:
-        ai_response = f"پاسخ به '{message}'"
-        update.message.reply_text(f"پاسخ AI: {ai_response}")
 
     def run(self):
         application = Application.builder().token(self.token).build()
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     bot = Bot.Bot("7906117866:AAFVuWuO0oU1tyAYPwWEEcN7W-3dsJhVq2k")
 
     bot.run()
+    print(bot.handle_message_in_background())
+    
+
 
 if __name__ == "__main__":
     main()
